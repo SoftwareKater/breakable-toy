52aa1d0758542ecdf2b1831378d1bc3e99f1331e
SoftwareKater <softwarekater@gmail.com>
2020-06-30 16:30:59 +0200

    style: disable linting rule no-empty-interface

--- libs/shared/data-access/mongo-storage/src/lib/resources/mongo-storage-module-options.entity.ts
+++ libs/shared/data-access/mongo-storage/src/lib/resources/mongo-storage-module-options.entity.ts
@@ -1,3 +1,4 @@
 import { MongoConnectionOptions } from './mongo-connection-options.entity';
 
+// tslint:disable-next-line:no-empty-interface
 export interface MongoStorageModuleOptions extends MongoConnectionOptions {}
